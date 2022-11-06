305376c070d924fd9b434bf5270c8e68ec1af835
kyungbeom Kim <kyungbeom8@naver.com>
2022-11-06 23:12:21 +0900

    11.06
    1. All files are modifed by system design
    2. Add Crawling function
    (KwCrawling(), CeCrawling, CsCrawling, IcCrawling)

--- noticeProgram.py
+++ noticeProgram.py
@@ -1,77 +1,8 @@
-import sys
-import csv
-from PyQt5.QtWidgets import *
-import webbrowser
-
-
-# PY 형식 파일 import
-from noticeUI import Ui_MainWindow
-from noticeScrapping import crawling
-
-
-# 프로그램 메인을 담당하는 Class 선언
-class MainClass(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        # UI 선언
-        main_ui = Ui_MainWindow()
-        # UI 준비
-        main_ui.setupUi(self)
-        self.setTableWidgetData(main_ui.tableWidget)
-        # 화면을 보여준다.
-        self.show()
-
-    def setTableWidgetData(self, tableWidget):
-        # csv 파일 정보를 list로 받아옴
-        noticeInfo = self.loadNoticeInfo()
-        # 행/열 개수 설정
-        tableWidget.setRowCount(len(noticeInfo))
-        tableWidget.setColumnCount(5)
-        # 행의 폭을 열 마다 다르게 조정
-        tableWidget.setColumnWidth(0, int(self.width() * 15 / 30))
-        tableWidget.setColumnWidth(1, int(self.width() * 3 / 30))
-        tableWidget.setColumnWidth(2, int(self.width() * 3 / 30))
-        tableWidget.setColumnWidth(3, int(self.width() * 4 / 30))
-        tableWidget.setColumnWidth(4, int(self.width() * 2 / 30))
-        # 열 이름 붙이는 부분®
-        colum_title = ["제목", "작성일", "수정일", "기재한 곳", "조회수"]
-        tableWidget.setHorizontalHeaderLabels(colum_title)
-        # 리스트 테이블 내용 입력
-        list_seq = 0
-        row = 0
-        column = 0
-        url = ""
-        for i in noticeInfo:
-            for j in i:
-                # url
-                if list_seq == 0:
-                    url = j
-                # 제목
-                elif list_seq == 1:
-                    label1 = QLabel()
-                    label1.setText('<a href="'+url+'">'+j+'</a>')
-                    label1.setOpenExternalLinks(True)
-                    tableWidget.setCellWidget(column, row, label1)
-                    row += 1
-                else:
-                    tableWidget.setItem(column, row, QTableWidgetItem(j))
-                    row += 1
-                list_seq += 1
-            list_seq = 0
-            row = 0
-            column += 1
-
-    # csv 파일로 부터 정보를 불러오는 부분
-    def loadNoticeInfo(self) -> list:
-        filename = "noticeTitle.csv"
-        f = open(filename, "r", encoding="utf-8-sig")
-        rdr = csv.reader(f)
-        a = []
-        for i in rdr:
-            a.append(i)
-        f.close()
-        return a
-
+from sys import argv
+from webbrowser import open as web_open
+from noticeUI import MainClass
+from PyQt5.QtWidgets import QApplication
+from noticeScrapping import *
 
 if __name__ == "__main__":
     # noticeTitle.csv 초기화
@@ -79,10 +10,14 @@ if __name__ == "__main__":
     f.close()
     # Crawling part
     for i in range(1, 20):
-        crawling(str(i))
-    # 광운대학교 로그인 사이트 open
-    webbrowser.open("https://klas.kw.ac.kr")
+        KwCrawling(str(i), "noticeTitle.csv")
+        CeCrawling(str(i), "CE.csv")
+        CsCrawling(str(i), "CS.csv")
+        IcCrawling(str(i), "IC.csv")
+
+    # 광운 대학교 로그인 사이트 open
+    web_open("https://klas.kw.ac.kr")
     # UI part
-    app = QApplication(sys.argv)
+    app = QApplication(argv)
     window = MainClass()
     app.exec_()
--- noticeScrapping.py
+++ noticeScrapping.py
@@ -4,10 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def crawling(pageNum):
+def KwCrawling(pageNum, filename):
     # Save result in csv
-    filename = "noticeTitle.csv"
-
     f = open(filename, "a", encoding="utf-8-sig", newline="")
     writer = csv.writer(f)
 
@@ -29,7 +27,7 @@ def crawling(pageNum):
     noticeArea = soup.find('div', 'list-box')
     noticeInfo = noticeArea.find_all('div', "board-text")
 
-    # [title/ url / 작성일 / 수정일 / 기재한 곳 / 조회수]를 행 단위로 csv파일에 입력 (9 line :writerow(list)사용)
+    # [url / title / 작성일 / 수정일 / 기재한 곳 / 조회수]를 행 단위로 csv파일에 입력 (9 line :writerow(list)사용)
 
     for notice in noticeInfo:
         colum= []
@@ -43,3 +41,145 @@ def crawling(pageNum):
         colum.append(noticeInfoList[9])
         colum.append(noticeInfoList[1])
         writer.writerow(colum)
+
+def CeCrawling(pageNum, filename):
+    # Save result in csv
+    f = open(filename, "a", encoding="utf-8-sig", newline="")
+    writer = csv.writer(f)
+
+    # Request url information from the Server
+    url = "https://ce.kw.ac.kr:501/department_office/lecture.php?page=" + pageNum + "&site_type=2&make=&search=&make=&search="
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+
+    }
+    # 서버와 코드 연결
+    res = requests.get(url, headers=headers)
+
+    # Check success or fail to load url information
+    res.raise_for_status()
+
+    # Make a soup instance (crawling part)
+    # lxml error 발생 시 lxml library 다운로드 받기
+    # 페이지의 원하는 부분을 특정하는 코드
+    soup = BeautifulSoup(res.text, "lxml")
+
+    noticeInfo = soup.find('table', "board-list").find("tbody").find_all('tr')
+
+    # 페이지가 존재하지 않는 경우 return
+    if not noticeInfo:
+        f.close()
+        return
+
+    # [url / title / 작성일 / 수정일 / 기재한 곳 / 조회수]를 행 단위로 csv파일에 입력 (9 line :writerow(list)사용)
+
+    for notice in noticeInfo:
+        colum = []
+        # url / 제목
+        if notice.find('td', "w_cell").find("img"):
+            colum.append("0")
+        else:
+            colum.append("1")
+        colum.append("https://ce.kw.ac.kr:501" + notice.find('td', "subject").find('a')['href'])
+        colum.append(notice.find('td', "subject").find('a').get_text().strip())
+        # 작성일 / 조회수
+        colum.append(notice.find('td', "w_date").get_text().strip())
+        colum.append(notice.select("td.w_cell")[1].get_text().strip())
+        writer.writerow(colum)
+
+    f.close()
+
+def CsCrawling(pageNum, filename):
+    # Save result in csv
+    f = open(filename, "a", encoding="utf-8-sig", newline="")
+    writer = csv.writer(f)
+
+    # Request url information from the Server
+    url = "https://cs.kw.ac.kr:501/department_office/lecture.php?page=" + pageNum + "&site_type=3&make=&search=&make=&search="
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+
+    }
+    # 서버와 코드 연결
+    res = requests.get(url, headers=headers)
+
+    # Check success or fail to load url information
+    res.raise_for_status()
+
+    # Make a soup instance (crawling part)
+    # lxml error 발생 시 lxml library 다운로드 받기
+    # 페이지의 원하는 부분을 특정하는 코드
+    soup = BeautifulSoup(res.text, "lxml")
+
+    noticeInfo = soup.find('table', "board-list").find("tbody").find_all('tr')
+
+    # 페이지가 존재하지 않는 경우 return
+    if not noticeInfo:
+        f.close()
+        return
+
+    # [url / title / 작성일 / 수정일 / 기재한 곳 / 조회수]를 행 단위로 csv파일에 입력 (9 line :writerow(list)사용)
+
+    for notice in noticeInfo:
+        colum = []
+        # url / 제목
+        if notice.find('td', "w_cell").find("img"):
+            colum.append("0")
+        else:
+            colum.append("1")
+        colum.append("https://cs.kw.ac.kr:501" + notice.find('td', "subject").find('a')['href'])
+        colum.append(notice.find('td', "subject").find('a').get_text().strip())
+        # 작성일 / 조회수
+        colum.append(notice.find('td', "w_date").get_text().strip())
+        colum.append(notice.select("td.w_cell")[1].get_text().strip())
+        writer.writerow(colum)
+
+    f.close()
+
+def IcCrawling(pageNum, filename):
+    # Save result in csv
+    f = open(filename, "a", encoding="utf-8-sig", newline="")
+    writer = csv.writer(f)
+
+    # Request url information from the Server
+    url = "https://ic.kw.ac.kr:501/department_office/lecture.php?page=" + pageNum + "&site_type=4&make=&search=&make=&search="
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+
+    }
+    # 서버와 코드 연결
+    res = requests.get(url, headers=headers)
+
+    # Check success or fail to load url information
+    res.raise_for_status()
+
+    # Make a soup instance (crawling part)
+    # lxml error 발생 시 lxml library 다운로드 받기
+    # 페이지의 원하는 부분을 특정하는 코드
+    soup = BeautifulSoup(res.text, "lxml")
+
+    noticeInfo = soup.find('table', "board-list").find("tbody").find_all('tr')
+
+    # 페이지가 존재하지 않는 경우 return
+    if not noticeInfo:
+        f.close()
+        return
+
+    # [url / title / 작성일 / 수정일 / 기재한 곳 / 조회수]를 행 단위로 csv파일에 입력 (9 line :writerow(list)사용)
+
+    for notice in noticeInfo:
+        colum = []
+        # url / 제목
+        if notice.find('td', "w_cell").find("img"):
+            colum.append("0")
+        else:
+            colum.append("1")
+        colum.append("https://ic.kw.ac.kr:501" + notice.find('td', "subject").find('a')['href'])
+        colum.append(notice.find('td', "subject").find('a').get_text().strip())
+        # 작성일 / 조회수
+        colum.append(notice.find('td', "w_date").get_text().strip())
+        colum.append(notice.select("td.w_cell")[1].get_text().strip())
+        writer.writerow(colum)
+
+    f.close()
+
